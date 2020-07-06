5a26e9cecf57ad6e0d61b90a8454c383da519b05
Runal <65166862+runalb@users.noreply.github.com>
2020-07-06 21:39:20 +0530

    .

---
+++ Programming for Everybody (Getting Started with Python)/Code/p8.py
@@ -0,0 +1,21 @@
+#Try / Except
+
+
+astr = "name"
+
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print("first ",istr)
+
+
+
+astr = "123"
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print("second", istr)
