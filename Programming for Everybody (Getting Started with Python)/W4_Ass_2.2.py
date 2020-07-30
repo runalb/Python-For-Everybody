b1c0f8abe1bb98878c4002668478eb676100faac
Runal <65166862+runalb@users.noreply.github.com>
2020-07-30 15:27:24 +0530

    Update W4_Ass_2.2.py

--- Programming for Everybody (Getting Started with Python)/W4_Ass_2.2.py
+++ Programming for Everybody (Getting Started with Python)/W4_Ass_2.2.py
@@ -1,12 +1,4 @@
-#2.3    Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will pop up a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
 
-
-hrs = input("Enter Hours:")
-hrs_f = float(hrs)
-
-rate = input("Enter Rate:")
-rate_f = float(rate)
-
-
-pay = hrs_f * rate_f
-print("Pay:" ,pay)
+name = input("Enter your name")
+print("Hello",name)
