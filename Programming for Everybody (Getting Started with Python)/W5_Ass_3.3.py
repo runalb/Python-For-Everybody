27c1b77c7bf6d339fa49ecc07e2f5302c48eb861
Runal <65166862+runalb@users.noreply.github.com>
2020-08-02 22:46:30 +0530

    .

---
+++ Programming for Everybody (Getting Started with Python)/W5_Ass_3.3.py
@@ -0,0 +1,33 @@
+# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+
+
+score = input("Enter Score: ")
+fscore = float(score)
+
+if fscore > 1.0:
+    print("out of range")
+
+elif fscore >= 0.9:
+    print("A")
+
+elif fscore >= 0.8:
+    print("B")
+
+elif fscore >= 0.7:
+    print("C")
+
+elif fscore >= 0.6:
+    print("D")
+
+elif fscore < 0.6 :
+    print("F")
+
+
+
