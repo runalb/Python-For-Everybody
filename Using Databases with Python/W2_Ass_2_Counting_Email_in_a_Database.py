1504b8fe3bbf8ea249b4af17931a9dfdca9d2936
Runal <65166862+runalb@users.noreply.github.com>
2020-08-29 15:42:16 +0530

    Counting_Email_in_a_Database.py

---
+++ Using Databases with Python/W2_Ass_2_Counting_Email_in_a_Database.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect('W2_Ass_2_emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('CREATE TABLE Counts(org TEXT, count INTEGER)')
+
+fname = 'mbox.txt'
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From: '):
+        continue
+    pieces = line.split()
+    email = pieces[1]
+    print("email:",email)
+    parts = email.split('@')
+    org = parts[-1]
+    print("edu:",org)
+    print()
+
+    cur.execute('SELECT count FROM Counts WHERE org = ?',(org,))
+    row = cur.fetchone()
+
+    if row is None:
+        cur.execute('INSERT INTO Counts (org,count) VALUES (?,1)',(org,))
+
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',(org,))
+    conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]),row[1])
+
+cur.close()
